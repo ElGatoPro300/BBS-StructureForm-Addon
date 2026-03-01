a3c059abfd2f493d540277d78c36ba4210d7c7c6
ElGatoPro300 <catgamer207@gmail.com>
2026-02-28 21:14:50 -0600

    adapt to 1.20.1

--- src/main/java/elgatopro300/bbsstructureform/client/forms/renderer/StructureFormRenderer.java
+++ src/main/java/elgatopro300/bbsstructureform/client/forms/renderer/StructureFormRenderer.java
@@ -871,7 +871,7 @@ public class StructureFormRenderer extends FormRenderer<StructureForm>
             || (b instanceof AttachedStemBlock)
             || state.isOf(Blocks.FERN)
             || state.isOf(Blocks.SUGAR_CANE)
-            || state.isOf(Blocks.SHORT_GRASS)
+            || state.isOf(Blocks.GRASS)
             || state.isOf(Blocks.TALL_GRASS)
             || state.isOf(Blocks.LARGE_FERN);
 
@@ -1037,7 +1037,7 @@ public class StructureFormRenderer extends FormRenderer<StructureForm>
         {
             try
             {
-                NbtCompound root = NbtIo.readCompressed(nbtFile.toPath(), NbtTagSizeTracker.ofUnlimitedBytes());
+                NbtCompound root = NbtIo.readCompressed(nbtFile);
 
                 this.parseStructure(root);
 
@@ -1052,7 +1052,7 @@ public class StructureFormRenderer extends FormRenderer<StructureForm>
         {
             try
             {
-                NbtCompound root = NbtIo.readCompressed(is, NbtTagSizeTracker.ofUnlimitedBytes());
+                NbtCompound root = NbtIo.readCompressed(nbtFile);
 
                 this.parseStructure(root);
             }
