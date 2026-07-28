4bfe1512833c2b30c750f99f50c7e8276d6e071b
ElGatoPro300 <catgamer207@gmail.com>
2026-07-27 20:56:33 -0600

    continue migration

--- src/main/java/elgatopro300/bbsstructureform/BBSSTRUCTUREFORMAddon.java
+++ src/main/java/elgatopro300/bbsstructureform/BBSSTRUCTUREFORMAddon.java
@@ -3,21 +3,39 @@ package elgatopro300.bbsstructureform;
 import elgatopro300.bbsstructureform.client.packs.WorldStructuresSourcePack;
 import elgatopro300.bbsstructureform.form.StructureForm;
 
+import mchorse.bbs_mod.BBSMod;
 import mchorse.bbs_mod.addons.BBSAddon;
+import mchorse.bbs_mod.events.Subscribe;
 import mchorse.bbs_mod.events.register.RegisterFormsEvent;
 import mchorse.bbs_mod.events.register.RegisterSourcePacksEvent;
 import mchorse.bbs_mod.resources.Link;
 import mchorse.bbs_mod.resources.packs.InternalAssetsSourcePack;
 
-public class BBSStructureFormAddon extends BBSAddon
+import net.fabricmc.api.ModInitializer;
+
+public class BBSStructureFormAddon extends BBSAddon implements ModInitializer
 {
     @Override
+    public void onInitialize()
+    {
+        try
+        {
+            BBSMod.getForms().register(Link.bbs("structure"), StructureForm.class);
+            BBSMod.getProvider().register(new InternalAssetsSourcePack("bbs-structureform", "assets/bbs_structureform", BBSStructureFormAddon.class));
+            BBSMod.getProvider().register(new WorldStructuresSourcePack());
+        }
+        catch (Throwable ignored) {}
+    }
+
+    @Override
+    @Subscribe
     protected void registerForms(RegisterFormsEvent event)
     {
         event.getForms().register(Link.bbs("structure"), StructureForm.class);
     }
 
     @Override
+    @Subscribe
     protected void registerSourcePacks(RegisterSourcePacksEvent event)
     {
         try
--- src/main/java/elgatopro300/bbsstructureform/BBSStructureFormClientAddon.java
+++ src/main/java/elgatopro300/bbsstructureform/BBSStructureFormClientAddon.java
@@ -1,26 +1,39 @@
 package elgatopro300.bbsstructureform;
 
+import elgatopro300.bbsaddonengine.BBSAddonEngineClient;
+import elgatopro300.bbsaddonengine.utils.HelperUIReplaysEditor;
 import elgatopro300.bbsstructureform.client.forms.renderer.StructureFormRenderer;
 import elgatopro300.bbsstructureform.client.gui.forms.UIStructureForm;
 import elgatopro300.bbsstructureform.form.StructureForm;
 import elgatopro300.bbsstructureform.importers.StructureImporter;
 
+import mchorse.bbs_mod.BBSModClient;
+import mchorse.bbs_mod.addons.AddonInfo;
 import mchorse.bbs_mod.addons.BBSClientAddon;
+import mchorse.bbs_mod.events.Subscribe;
 import mchorse.bbs_mod.events.register.RegisterFormCategoriesEvent;
 import mchorse.bbs_mod.events.register.RegisterFormsRenderersEvent;
 import mchorse.bbs_mod.events.register.RegisterImportersEvent;
 import mchorse.bbs_mod.events.register.RegisterL10nEvent;
 import mchorse.bbs_mod.forms.FormCategories;
 import mchorse.bbs_mod.forms.categories.FormCategory;
+import mchorse.bbs_mod.forms.FormUtilsClient;
+import mchorse.bbs_mod.importers.Importers;
 import mchorse.bbs_mod.resources.Link;
+import mchorse.bbs_mod.ui.forms.editors.UIFormEditor;
 import mchorse.bbs_mod.ui.utils.icons.Icons;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
 import java.util.Collections;
+import java.util.List;
 
-import elgatopro300.bbsaddonengine.utils.HelperUIReplaysEditor;
-
-public class BBSStructureFormClientAddon extends BBSClientAddon
+public class BBSStructureFormClientAddon extends BBSClientAddon implements ClientModInitializer
 {
+    private static boolean L10N_DONE = false;
+
     public BBSStructureFormClientAddon()
     {
         try
@@ -37,6 +50,105 @@ public class BBSStructureFormClientAddon extends BBSClientAddon
     }
 
     @Override
+    public void onInitializeClient()
+    {
+        try
+        {
+            FormUtilsClient.register(StructureForm.class, StructureFormRenderer::new);
+            UIFormEditor.register(StructureForm.class, UIStructureForm::new);
+        }
+        catch (Throwable ignored) {}
+
+        ClientTickEvents.END_CLIENT_TICK.register(client ->
+        {
+            if (!L10N_DONE && BBSModClient.getL10n() != null)
+            {
+                try
+                {
+                    BBSModClient.getL10n().register((lang) -> Collections.singletonList(new Link("bbs-structureform", "lang/" + lang + ".json")));
+                    BBSModClient.getL10n().reload();
+                    Importers.getImporters().add(new StructureImporter());
+                }
+                catch (Throwable ignored) {}
+                L10N_DONE = true;
+            }
+
+            if (BBSModClient.getFormCategories() != null)
+            {
+                addStructurePreset(BBSModClient.getFormCategories());
+            }
+        });
+
+        ClientLifecycleEvents.CLIENT_STARTED.register(client ->
+        {
+            try
+            {
+                FormUtilsClient.register(StructureForm.class, StructureFormRenderer::new);
+                UIFormEditor.register(StructureForm.class, UIStructureForm::new);
+
+                if (BBSModClient.getFormCategories() != null)
+                {
+                    addStructurePreset(BBSModClient.getFormCategories());
+                }
+            }
+            catch (Throwable ignored) {}
+        });
+    }
+
+    private static void addStructurePreset(FormCategories categories)
+    {
+        if (categories == null || categories.getAllCategories() == null || categories.getAllCategories().isEmpty())
+        {
+            return;
+        }
+
+        FormCategory targetCategory = null;
+
+        for (FormCategory category : categories.getAllCategories())
+        {
+            if (category != null && category.title != null)
+            {
+                String val = String.valueOf(category.title.get()).toLowerCase();
+
+                if (val.contains("extra") || val.contains("miscel") || val.contains("misc"))
+                {
+                    targetCategory = category;
+                    break;
+                }
+            }
+        }
+
+        if (targetCategory == null)
+        {
+            targetCategory = categories.getAllCategories().get(0);
+        }
+
+        if (targetCategory != null)
+        {
+            boolean exists = false;
+            if (targetCategory.getForms() != null)
+            {
+                for (Object f : targetCategory.getForms())
+                {
+                    if (f instanceof StructureForm)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!exists)
+            {
+                StructureForm form = new StructureForm();
+                form.structureFile.set("bbs-structureform:structures/tree.nbt");
+                targetCategory.addForm(form);
+            }
+        }
+    }
+
+    @Override
+    @Subscribe
     protected void registerFormsRenderers(RegisterFormsRenderersEvent event)
     {
         event.registerRenderer(StructureForm.class, StructureFormRenderer::new);
@@ -44,44 +156,27 @@ public class BBSStructureFormClientAddon extends BBSClientAddon
     }
 
     @Override
+    @Subscribe
+    protected void registerFormCategories(RegisterFormCategoriesEvent event)
+    {
+        addStructurePreset(event.getCategories());
+    }
+
+    @Override
+    @Subscribe
     protected void registerImporters(RegisterImportersEvent event)
     {
         event.register(new StructureImporter());
     }
 
     @Override
+    @Subscribe
     protected void registerL10n(RegisterL10nEvent event)
     {
         try
         {
             event.l10n.register((lang) -> Collections.singletonList(new Link("bbs-structureform", "lang/" + lang + ".json")));
             event.l10n.reload();
-        }
-        catch (Throwable ignored) {}
-    }
-
-    @Override
-    protected void registerFormCategories(RegisterFormCategoriesEvent event)
-    {
-        try
-        {
-            FormCategories categories = event.getCategories();
-            if (categories != null && categories.getAllCategories() != null)
-            {
-                for (FormCategory category : categories.getAllCategories())
-                {
-                    if (category != null && category.title != null && category.title.get() != null)
-                    {
-                        if (category.title.get().toLowerCase().contains("extra"))
-                        {
-                            StructureForm form = new StructureForm();
-                            form.structureFile.set("bbs-structureform:structures/tree.nbt");
-                            category.addForm(form);
-                            break;
-                        }
-                    }
-                }
-            }
         }
         catch (Throwable ignored) {}
     }
